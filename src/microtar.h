1526fe58d7018cc8ec89c3aa7b46775f81ba3b7d
sxv512 <sxv512@cs.bham.ac.uk>
2017-03-05 23:26:28 +0000

    Changed license

--- src/microtar.h
+++ src/microtar.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017 rxi
+ * Copyright (c) 2017 c0mpute
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the MIT license. See `microtar.c` for details.
